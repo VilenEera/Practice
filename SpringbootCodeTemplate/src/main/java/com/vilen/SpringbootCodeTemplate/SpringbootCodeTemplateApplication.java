57c78a3ec703f1f02ac3c63e62f2915452971ad8
chhx4567 <chhx4567@163.com>
2017-10-18 12:16:46 +0800

    init CodeTemplate

---
+++ SpringbootCodeTemplate/src/main/java/com/vilen/SpringbootCodeTemplate/SpringbootCodeTemplateApplication.java
@@ -0,0 +1,12 @@
+package com.vilen.SpringbootCodeTemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCodeTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCodeTemplateApplication.class, args);
+	}
+}
