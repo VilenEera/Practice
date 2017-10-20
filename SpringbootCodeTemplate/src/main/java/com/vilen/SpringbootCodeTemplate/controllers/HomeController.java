4bfa893e15924985355b62162fd10ea056e8a6cf
chhx4567 <chhx4567@163.com>
2017-10-20 19:49:31 +0800

    index html 完成

---
+++ SpringbootCodeTemplate/src/main/java/com/vilen/SpringbootCodeTemplate/controllers/HomeController.java
@@ -0,0 +1,30 @@
+package com.vilen.SpringbootCodeTemplate.controllers;
+
+import com.vilen.SpringbootCodeTemplate.beans.User;
+import com.vilen.common.beans.ResultBean;
+import com.vilen.common.utils.UserUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+
+/**
+ * Created by vilen on 17/10/20.
+ */
+@RestController
+public class HomeController {
+    private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+
+    @PostMapping("/login")
+    public ResultBean<User> login(HttpSession session, String username) {
+        logger.info("login user:" + username);
+        // 模拟登陆
+        User user = new User(100L, username, "nick");
+        session.setAttribute(UserUtil.KEY_USER,user);
+        return new ResultBean<User>(user);
+    }
+
+
+}
