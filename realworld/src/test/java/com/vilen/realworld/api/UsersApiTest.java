ba6efe5f6d3d61f6d41ba8682482101ced546c95
chhx4567 <chhx4567@163.com>
2017-10-25 02:15:35 +0800

    program() user api test

--- realworld/src/test/java/com/vilen/realworld/api/UsersApiTest.java
+++ realworld/src/test/java/com/vilen/realworld/api/UsersApiTest.java
@@ -114,4 +114,125 @@ public class UsersApiTest {
                 .statusCode(422)
                 .body("errors.username[0]", equalTo("can't be empty"));
     }
+
+    @Test
+    public void should_show_error_message_for_invalid_email() throws Exception {
+        String email = "johnxjacob.com";
+        String username = "johnjacob";
+
+        Map<String, Object> param = prepareRegisterParameter(email, username);
+        given()
+                .contentType("application/json")
+                .body(param)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(422)
+                .body("errors.email[0]", equalTo("should be an email"));
+    }
+
+    @Test
+    public void should_show_error_for_duplicated_username() throws Exception{
+        String email = "john@jacob.com";
+        String username = "johnjacob";
+
+        when(userRepository.findByUsername(eq(username))).thenReturn(Optional.of(new User(email, username, "123", "bio", "")));
+        when(userRepository.findByEmail(any()))
+                .thenReturn(Optional.empty());
+
+        Map<String, Object> param = prepareRegisterParameter(email, username);
+        given()
+                .contentType("application/json")
+                .body(param)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(422)
+                .body("errors.username[0]", equalTo("duplicated username"));
+    }
+
+    @Test
+    public void should_show_error_for_duplicated_email() throws Exception {
+        String email = "john@jacob.com";
+        String username = "johnjacob2";
+
+        when(userRepository.findByEmail(eq(email))).thenReturn(Optional.of(new User(
+                email, username, "123", "bio", ""
+        )));
+
+        when(userRepository.findByUsername(eq(username))).thenReturn(Optional.empty());
+
+        Map<String, Object> param = prepareRegisterParameter(email, username);
+
+        given()
+                .contentType("application/json")
+                .body(param)
+                .when()
+                .post("/users")
+                .then()
+                .statusCode(422)
+                .body("errors.email[0]", equalTo("duplicated email"));
+    }
+
+    @Test
+    public void should_login_success() throws Exception {
+        String email = "john@jacob.com";
+        String username = "johnjacob2";
+        String password = "123";
+        User user = new User(email, username, password, "", defaultAvatar);
+        UserData userData = new UserData("123", email, username, "", defaultAvatar);
+        when(userRepository.findByEmail(eq(email))).thenReturn(Optional.of(user));
+        when(userReadService.findByUsername(eq(username))).thenReturn(userData);
+        when(userReadService.findById(eq(user.getId()))).thenReturn(userData);
+        when(jwtService.toToken(any())).thenReturn("123");
+
+        Map<String, Object> param = new HashMap<String, Object>(){
+            {
+                put("user",new HashMap<String,Object>() {{
+                    put("email", email);
+                    put("password", password);
+                }});
+            }};
+        given()
+                .contentType("application/json")
+                .body(param)
+                .when()
+                .post("/users/login")
+                .then()
+                .statusCode(200)
+                .body("user.email", equalTo(email))
+                .body("user.username", equalTo(username))
+                .body("user.bio", equalTo(""))
+                .body("user.image", equalTo(defaultAvatar))
+                .body("user.token", equalTo("123"));
+    }
+
+    @Test
+    public void should_fail_login_with_wrong_password() throws Exception {
+        String email = "john@jacob.com";
+        String username = "johnjacob2";
+        String password = "123";
+
+        User user = new User(email, username, password, "", defaultAvatar);
+        UserData userData = new UserData(user.getId(), email, username, "", defaultAvatar);
+
+        when(userRepository.findByEmail(eq(email))).thenReturn(Optional.of(user));
+        when(userReadService.findByUsername(eq(username))).thenReturn(userData);
+
+        Map<String, Object> param = new HashMap<String, Object>() {{
+            put("user", new HashMap<String, Object>() {{
+                put("email", email);
+                put("password", "123123");
+            }});
+        }};
+
+        given()
+                .contentType("application/json")
+                .body(param)
+                .when()
+                .post("/users/login")
+                .then()
+                .statusCode(422)
+                .body("errors.password[0]", equalTo("invalid email or password"));
+    }
 }
