2a38d5b6851a594e8943983c2cae0b723170884a
chhx4567 <chhx4567@163.com>
2017-10-27 18:16:40 +0800

    fix() cors

---
+++ realworld/src/main/java/com/vilen/realworld/api/security/CORSConfig.java
@@ -0,0 +1,17 @@
+package com.vilen.realworld.api.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by vilen on 17/10/27.
+ */
+@Configuration
+public class CORSConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+    }
+}
